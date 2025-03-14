3be0c73170bb3bb4b5e3012e4bb09f5567146e15
terryktee <ttkapumhaa@gmail.com>
2025-03-14 14:56:56 +0200

    first commit

--- API/campuskeyapi/users/models.py
+++ API/campuskeyapi/users/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,Permiss
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, usernae, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        username = self.normalize_email(username)
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -18,25 +18,22 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         return self.create_user(email, password, **extra_fields)
 
-    def get_by_natural_key(self, email):  
-        return self.get(email=email)
-
 class custom_user(AbstractBaseUser, PermissionsMixin):
     user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permission", blank=True)
     groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
-    email = models.EmailField(unique=True)
+    user = models.EmailField(unique=True)
     is_student = models.BooleanField(default=False)
     is_agent = models.BooleanField(default=False)
     is_landlord = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()  # 
 
     def __str__(self):
-        return self.email
+        return self.username
 
 class Agent(models.Model):
     user=models.OneToOneField(custom_user,on_delete=models.CASCADE)
