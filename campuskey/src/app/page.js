4cc3c6df6a11e7e56da97a9ade6eb3e93d743bf9
terryktee <ttkapumhaa@gmail.com>
2025-03-17 03:55:29 +0200

    creating login ui

--- campuskey/src/app/page.js
+++ campuskey/src/app/page.js
@@ -1,87 +1,96 @@
-import Nav from "./components/nav"
-import Footer from "./components/footer"
-export default function Home() {
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {logo} from "../../public/logo.jpg"
+export default function LoginPage({
+  className,
+  ...props
+}) {
   return (
-    <div>
-      {/* Navigation Code*/}
-      <Nav />
-      {/* Main Container Code*/}
-      <main>
-        {/* Searchbar Code*/}
-        <div className="search-box">
-          <h1>Find your off-campus home</h1>
-          <div className="search">
-            <input type="search" name="search" placeholder="search by location or university"></input>
-            <button>Search</button>
-          </div>
-        </div>
-        {/* filter Code*/}
-        <div className="filter-box">
-          <select>
-            <option>Property type</option>
-            <option>Boys Only</option>
-            <option>Girls Only</option>
-            <option>Mixed</option>
-          </select>
-          <select>
-            <option>Distance</option>
-          </select>
-          <select>
-            <option>Price Range</option>
-          </select>
-          <select>
-            <option>More filters</option>
-          </select>
-          <div className="sort">
-            <p>Sort by:</p>
-            <select>
-              <option>Recommendation</option>
-            </select>
-          </div>
-        </div>
-        {/* Properties Code*/}
-        <div>
-          <div className="left-box">
-            <h1>Facilities</h1>
-           <label><input type="checkbox"/> wifi</label> 
-           <label><input type="checkbox"/> Back up Zesa</label> 
-           <label><input type="checkbox"/> Backup Water</label> 
-           <label><input type="checkbox"/> Gas Stove</label> 
-           <label><input type="checkbox"/> Beds</label> 
-           <label><input type="checkbox"/> Fridge</label> 
-           <label><input type="checkbox"/> Insuit</label> 
-           <label><input type="checkbox"/> Chair and Desk</label> 
-            <h1>Services</h1>
-            <label><input type="checkbox"/> Security</label> 
-            <label><input type="checkbox"/>Care taker</label> 
-            <h1>Room Type</h1>
-            <input type="radio"/><label>Room of 1</label> 
-            <input type="radio"/><label>Room of 2</label> 
-            <input type="radio"/><label>Room of 3</label> 
-            <input type="radio"/><label>room for more than 3</label> 
-          </div>
-          <div className="right-box">
-            <h1>Available off-campus houses</h1>
-            <div className="">
-              <div className="">
-                <img src="image" alt="House Image"></img>
-                {/* OverView of Verified or not*/}
-                <h1>Off-Campus title + price</h1>
-                <h1>Distance</h1>
-                <h1>Room type</h1>
-                <h1>Bathrooms</h1>
-                <h1>Facilities + services </h1>
-                <h1>Stars</h1>
-                <button>View Details</button>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <div className={cn("flex flex-col gap-6", className)}>
+          <Card className="overflow-hidden">
+            <CardContent className="grid p-0 md:grid-cols-2">
+              <form className="p-6 md:p-8">
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col items-center text-center">
+                    <h1 className="text-2xl font-bold text-[#0D6CA3]">Welcome back</h1>
+                    <p className="text-balance text-muted-foreground">
+                      Login to your CampusKey Account
+                    </p>
+                  </div>
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                      <a
+                        href="#"
+                        className="ml-auto text-sm underline-offset-2 hover:underline text-[#0D6CA3]"
+                      >
+                        Forgot your password?
+                      </a>
+                    </div>
+                    <Input id="password" type="password" required />
+                    <a
+                        href="#"
+                        className="ml-auto text-sm underline-offset-2 hover:underline text-[#0D6CA3]"
+                      >
+                        Verify email
+                      </a>
+                  </div>
+                  <Button type="submit" className="w-full bg-[#0D6CA3]">
+                    Sign In
+                  </Button>
+                  <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                      Or continue with
+                    </span>
+                  </div>
+                  <div>
+                    <Button variant="outline" className="w-full">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                          d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                      <span className="sr-only">Login with Google</span>
+                    </Button>
+                  </div>
+                  <div className="text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <a href="#" className="underline underline-offset-4 text-[#0D6CA3]">
+                      Sign Up
+                    </a>
+                  </div>
+                </div>
+              </form>
+              <div className="relative hidden bg-muted md:block">
+                <img
+                  src="/logo.jpg"
+                  alt="Image"
+                  className="absolute inset-0 h-full w-full object-fit dark:brightness-[0.2] dark:grayscale"
+                />
               </div>
-            </div>
+            </CardContent>
+          </Card>
+          <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+            By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+            and <a href="#">Privacy Policy</a>.
           </div>
         </div>
-      </main>
-      {/* footer Code*/}
-      <footer>
-      <Footer />
-      </footer>
+      </div>
     </div>
-  );
+  )
 }
