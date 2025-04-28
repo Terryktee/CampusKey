e759c7de34e2f00e3528b259b47a41bab9d73268
terryktee <ttkapumhaa@gmail.com>
2025-04-28 11:36:55 +0200

    Weekly update

---
+++ campuskey/src/app/application/page.js
@@ -0,0 +1,12 @@
+export default function Application(){
+return(
+    <div className="flex justify-center bg-red-300">
+        <div className="flex flex-col bg-green-300">
+            <div className="w-[740px]">
+                <h1>Student Name</h1>
+            </div>
+        </div>
+    </div>
+)
+
+}
