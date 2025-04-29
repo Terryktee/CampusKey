602bfd1f02f077e8fc1fe7ad6083a029b7257757
terryktee <ttkapumhaa@gmail.com>
2025-04-29 14:56:25 +0200

    daily update

--- campuskey/src/app/application/page.js
+++ campuskey/src/app/application/page.js
@@ -1,12 +1,21 @@
+import Nav from "../components/nav";
+import { Button } from "@/components/ui/button";
 export default function Application(){
 return(
-    <div className="flex justify-center bg-red-300">
-        <div className="flex flex-col bg-green-300">
-            <div className="w-[740px]">
-                <h1>Student Name</h1>
+    <>
+    <Nav />
+    <div className="flex justify-center ">
+        <div className="flex flex-col ">
+            <div className="w-[740px] rounded-lg shadow-lg p-2">
+                <h1>Title : Student Name : House Owner</h1>
+                <h3>Subtitle : Student Number : House Number</h3>
+                <div>
+                    <Button > Accept | View | Cancel | </Button>
+                </div>
             </div>
         </div>
     </div>
+    </>
 )
 
 }
